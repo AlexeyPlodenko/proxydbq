9e2127b12da1257d2dcd708b0544bfe6788800e9
Aleksejs Plodenko <alexey@plodenko.com>
2026-05-13 19:13:09 +0300

    Disables artifacts upload.

--- forge.config.mjs
+++ forge.config.mjs
@@ -67,7 +67,7 @@ export default {
                     name: 'proxydbq',
                 },
                 prerelease: false,
-                draft: true,
+                draft: false,
             },
         },
     ],
